a24ea881760fe846c78ae946fdfd631383310e0a
Peter Fänge <pellefange@gmail.com>
2024-11-24 17:49:12 +0100

    added grid-gap

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,18 @@
-import {Lotr, Interstellar, Dune2, PrincessMononoke, DarkKnight, LawrenceOfArabia} from "./Components/movies.jsx"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home.jsx";
+import Counter from "./pages/Counter.jsx";
+import Contact from "./pages/Contact.jsx";
 
 function App() {
   return (
     <>
-      <Lotr/>
-      <Interstellar/>
-      <Dune2/>
-      <PrincessMononoke/>
-      <DarkKnight/>
-      <LawrenceOfArabia/>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<Home/>} />
+          <Route path = "/counter" element = {<Counter/>} />
+          <Route path = "/contact" element = {<Contact/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
