778e9ce6aa45a3ead8791e360ee3de558fc9cddc
Peter Fänge <pellefange@gmail.com>
2024-11-24 17:52:29 +0100

    added navbar to function

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home.jsx";
 import Counter from "./pages/Counter.jsx";
 import Contact from "./pages/Contact.jsx";
+import Navbar from "./navbar.jsx";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar></Navbar>
         <Routes>
           <Route path = "/" element = {<Home/>} />
           <Route path = "/counter" element = {<Counter/>} />
