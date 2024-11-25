a348abbd50eb09e1c485e5ed464c75fd0137578f
Peter Fänge <pellefange@gmail.com>
2024-11-25 21:53:45 +0100

    Simple footer is now included, need to add some links in footer

---
+++ my-react-app/src/footer.jsx
@@ -0,0 +1,17 @@
+import {Link} from "react-router-dom";
+
+import "./footer.css";
+
+function Footer () {
+    return (
+        <div className = "footer-container">
+            <div className = "social-btn-container">
+                <Link><p>Placeholder</p></Link>
+                <Link><p>Placeholder</p></Link>
+                <Link><p>Placeholder</p></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
