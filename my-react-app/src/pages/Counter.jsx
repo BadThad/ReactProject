90048c4caa2f9d9f501926a8236929dd71879ca7
Peter Fänge <pellefange@gmail.com>
2024-11-25 21:32:58 +0100

    added Counter page with functions for counter

--- my-react-app/src/pages/Counter.jsx
+++ my-react-app/src/pages/Counter.jsx
@@ -1,10 +1,31 @@
+import {useState} from "react";
 
+import "../Counter.css"
 
 function Counter() {
+    const [counter, setCounter] = useState (10); /* Counter is set with initial state of 10 */
+
+    const increaseCount = () => {
+        setCounter(counter + 1);
+    }
+
+    const decreaseCount = () => {
+        setCounter(counter - 1);
+    }
+
+    const resetCount = () => {
+        setCounter(10);
+    }
+
     return ( 
-        <dev>
-            Counter
-        </dev>
+        <div className = "counter-container">
+            <h2>{counter}</h2>
+            <div className="counter-btn">
+                <button className="decrease-btn" onClick={decreaseCount}>-</button>
+                <button className="increase-btn" onClick={increaseCount}>+</button>
+            </div>
+            <button className="reset-btn" onClick={resetCount}>Reset</button>
+        </div>
     )
 }
 
