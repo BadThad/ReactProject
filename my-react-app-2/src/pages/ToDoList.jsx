e97640b51576a041d34352926c099ee86d5502c7
Peter Fänge <pellefange@gmail.com>
2024-12-10 21:38:37 +0100

    removed getElementByID and implemented an onChange event instead and added comments

--- my-react-app-2/src/pages/ToDoList.jsx
+++ my-react-app-2/src/pages/ToDoList.jsx
@@ -5,32 +5,46 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import "../ToDoList.css";
 
 function ToDoList () {
-    const [list, setList] = useState([])
+    const [list, setList] = useState([]);               // A useState with an array for list of Todo items.
+    const [inputValue, setInputValue] = useState("");   // A useState to manage user input with an initial state of an empty string.
 
-        function handleAddItem () {
-            const newItem = document.getElementById("itemInput").value;
-            document.getElementById("itemInput").value = "";
-
-            setList ((list) =>[...list, newItem]);
+        // Function which tracks the value in the input field.
+        function handleInput (e) {
+            setInputValue(e.target.value);
         }
 
-        function handleRemoveItem (index) {
-            setList(list.filter((_, i) => i !== index))
+        // Function which handles adding an item to the ToDo list.
+        function handleAddItem () {         
+
+            setList ([...list, inputValue]);    // This updates and expands the ToDo list array using a spread (...) syntax and the value the user inputs.
+            setInputValue("");                  // Clears the input field on button click.
         }
+
+        // Function which removes a ToDo list item.
+        function handleRemoveItem (index) {                 
+            setList(list.filter((_, i) => i !== index));    // Updates the list by creating a new array using the filter method.
+        }                                                   // This new array will not contain the user-added input element connected to the delete button.
 
     return (
         <div className="todolist-container">
             <h2 className="todolist-title">My ToDo List</h2>
             <ul>
-                {list.map((task, index) => (
-                    <li key={index}> {task}
+                {list.map((list, index) => (
+                    <li key={index}> {list}
                         <button className="delete-task-btn" onClick={ () => handleRemoveItem(index)}>
                             <FontAwesomeIcon icon={faTrash}/>
                         </button>
                     </li>   
                 ))}
             </ul>
-            <input type="text" placeholder="Type what you need to do..." id="itemInput"/>
+            <label>
+                <input 
+                    type="text" 
+                    placeholder="Type what you need to do..." 
+                    value = {inputValue}
+                    onChange={handleInput}
+                />
+            </label>
             <button className="add-item-btn" onClick={handleAddItem}>Add to list</button>
         </div>
     );
